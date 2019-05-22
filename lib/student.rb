0459d61d99a9b9eb851c3f46cbd460cd997802b2
Brendan Brown <pbbrown1112@gmail.com>
2019-05-22 17:28:09 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,22 @@
-class Student 
+class Student < User
+
+    attr_reader :knowledge
+    
+
+    def initialize
+        @knowledge = []
+       
+    end
+
+    def learn(info)
+        @knowledge << info
+              
+    end
+
+    def knowledge
+        @knowledge
+    end
+
+
 
 end
